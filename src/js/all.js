c1e6a315a6ea313c87a78931d973652b314aedf1
Aurora <kid321p@gmail.com>
2018-05-04 22:52:12 +0900

    ひととおり

--- src/js/all.js
+++ src/js/all.js
@@ -1,32 +1,55 @@
 $(function() {
 
   $('#submit').on('click', function() {
+    $(this).prop('disabled', true);
 
     const params = {
       title: $('#title').val(),
-      publishedYear: $('#year').val(),
+      publishedYear: parseInt($('#year').val()),
       genre: $('#genre').val(),
-      score: $('#score').val(),
+      score: parseInt($('#score').val()),
       review: $('#review').val()
     };
 
+    if (!validate(params)) {
+      $(this).prop('disabled', false);
+      return false;
+    }
     $.ajax({
       url: 'https://us-central1-myownvoiceisenough.cloudfunctions.net/postMovie',
       type: 'POST',
       data: params
     })
     .done(function(data, status, XHR) {
-        // ...
-      console.log('succeedddddeed');
+      alert('succeeded!!');
+      $('#submit').prop('disabled', false);
     })
     .fail(function(XHR, status, e) {
-        // ...
-      console.log('ova');
-    })
-    .always(function(XHR, status) {
-        // ...
+      alert('failed...');
+      $('#submit').prop('disabled', false);
     });
 
   });
 
+  $('#cancel').on('click', function() {
+    $('#submit').prop('disabled', false);
+  });
+
+  function validate(params) {
+    let msg = "";
+    if (params.title === "") {
+      msg += "Title,";
+    }
+    if (isNaN(params.score) || score < 0 || score > 100) {
+      msg += "Score, ";
+    }
+    if (params.review === "") {
+      msg += "Review";
+    }
+    if (msg !== "") {
+      alert('入力不足 ' + msg);
+      return false;
+    }
+    return true;
+  };
 });
