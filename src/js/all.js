a530fee8f0be7286db677efaebfed803e3af3092
Aurora <kid321p@gmail.com>
2018-05-04 15:31:55 +0900

    making form

---
+++ src/js/all.js
@@ -0,0 +1,10 @@
+$(function() {
+  const params = {
+    title: $('#title').val(),
+    publishedYear: $('#year').val(),
+    genre: $('#genre').val(),
+    score: $('#score').val(),
+    review: $('#review').val()
+  };
+
+});
