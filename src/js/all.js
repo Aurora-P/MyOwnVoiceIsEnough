613fe6cc327b6a20a4a4d9f19925250a8239f39e
Aurora <kid321p@gmail.com>
2018-05-04 16:07:12 +0900

    modified and deleted

--- src/js/all.js
+++ src/js/all.js
@@ -1,10 +1,32 @@
 $(function() {
-  const params = {
-    title: $('#title').val(),
-    publishedYear: $('#year').val(),
-    genre: $('#genre').val(),
-    score: $('#score').val(),
-    review: $('#review').val()
-  };
+
+  $('#submit').on('click', function() {
+
+    const params = {
+      title: $('#title').val(),
+      publishedYear: $('#year').val(),
+      genre: $('#genre').val(),
+      score: $('#score').val(),
+      review: $('#review').val()
+    };
+
+    $.ajax({
+      url: 'https://us-central1-myownvoiceisenough.cloudfunctions.net/postMovie',
+      type: 'POST',
+      data: params
+    })
+    .done(function(data, status, XHR) {
+        // ...
+      console.log('succeedddddeed');
+    })
+    .fail(function(XHR, status, e) {
+        // ...
+      console.log('ova');
+    })
+    .always(function(XHR, status) {
+        // ...
+    });
+
+  });
 
 });
